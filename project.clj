275c6e84b3d29e54ddb8acdfe8398a2b0dd6fbad
Rhodri Pugh <rod@pu-gh.com>
2014-08-26 12:05:04 +0100

    version 0.2.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject scotam/funnel "0.1.0"
+(defproject scotam/funnel "0.2.0"
   :description "Ring Request Funnel"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]])
